8b3145603104c6f67915c52840dd5c46962f1c15
fcy <fuchaoyang@lianjia.com>
2016-01-31 01:01:54 +0800

    更改图片

--- src/org/jaaksi/view/fcy1/MyActivity.java
+++ src/org/jaaksi/view/fcy1/MyActivity.java
@@ -2,35 +2,33 @@ package org.jaaksi.view.fcy1;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 import org.jaaksi.view.fcy1.view.MyRatingBar;
 
-public class MyActivity extends Activity {
+public class MyActivity extends Activity
+{
 
-    private MyRatingBar ratingBar;
+	private MyRatingBar ratingBar;
 
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.ratingbar);
+	/**
+	 * Called when the activity is first created.
+	 */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ratingbar);
 
 		ratingBar = (MyRatingBar) findViewById(R.id.rating_var);
 		ratingBar.setRatings(3.5f);
-        ratingBar.setRating(2);
+		ratingBar.setRating(2);
 		ratingBar.setRatings(4.5f);
 	}
 
-        Log.i("fcy","----------------- i="+i);
-    }
-
-    public void getRating(View view) {
-        int rating = ratingBar.getRating();
-        Toast.makeText(this,"rating="+rating,Toast.LENGTH_SHORT).show();
-    }
+	public void getRating(View view)
+	{
+		int rating = ratingBar.getRating();
+		Toast.makeText(this, "rating=" + rating, Toast.LENGTH_SHORT).show();
+	}
 }
